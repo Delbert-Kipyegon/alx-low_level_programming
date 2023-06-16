1e9c2973b7c49688cf9e03e4284407b86aa6b6a2
Delbert-Kipyegon <delbertkip@gmail.com>
2023-06-16 14:38:30 -0700

    numberz + putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+for (num = 0 ; num  <= 9 ; num++)
+{
+	putchar(digit + '0');
+}
+
+putchar('\n');
+
+return (0);
+}
