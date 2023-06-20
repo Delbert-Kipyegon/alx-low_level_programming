f012e8f87f2e611acfabca4823181ae224ebf0a3
Delbert-Kipyegon <delbertkip@gmail.com>
2023-06-20 11:54:18 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * printFibonacci - Prints the first n Fibonacci numbers
+ * @n: The number of Fibonacci numbers to print
+ *
+ * Prints the first n Fibonacci numbers, separated by commas and spaces.
+ */
+
+void printFibonacci(int n)
+{
+	int fib[50];
+
+	fib[0] = 1;
+	fib[1] = 2;
+
+	for (int i = 2; i < n; i++)
+	{
+		fib[i] = fib[i - 1] + fib[i - 2];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d", fib[i]);
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
