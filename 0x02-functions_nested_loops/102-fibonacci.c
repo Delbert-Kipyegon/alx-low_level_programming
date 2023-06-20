305c9f2c3a1a3f1d1ebb49603b25b62e97360bbf
Delbert-Kipyegon <delbertkip@gmail.com>
2023-06-20 11:57:09 -0700

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,16 +10,17 @@
 void printFibonacci(int n)
 {
 	int fib[50];
+	int i;
 
 	fib[0] = 1;
 	fib[1] = 2;
 
-	for (int i = 2; i < n; i++)
+	for (i = 2; i < n; i++)
 	{
 		fib[i] = fib[i - 1] + fib[i - 2];
 	}
 
-	for (int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", fib[i]);
 		if (i != n - 1)
