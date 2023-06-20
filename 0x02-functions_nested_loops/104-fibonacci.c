573a8b60f8be693ba3d7e438d7923c835c81410d
Delbert-Kipyegon <delbertkip@gmail.com>
2023-06-20 12:34:28 -0700

    first 98  fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,60 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * fibonacci_sequence - Generates Fibonacci sequence up to n numbers
- * @n: The number of Fibonacci numbers to generate
- *
- * Return: An array containing the Fibonacci sequence
- */
-int *fibonacci_sequence(int n)
-{
-	int *sequence;
-	int i;
-
-	if (n <= 0)
-		return (NULL);
-
-	sequence = malloc(n * sizeof(int));
-	if (sequence == NULL)
-		return (NULL);
-
-	sequence[0] = 1;
-	if (n > 1)
-		sequence[1] = 2;
-
-	for (i = 2; i < n; i++)
-		sequence[i] = sequence[i - 1] + sequence[i - 2];
-
-	return (sequence);
-}
-
-/**
- * main - Entry point of the program
- *
- * Return: 0 on success
+ * main - finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2
+ * followed by a new line
+ * Return: ALways 0 (Success)
  */
 int main(void)
 {
-	int *fibonacci_numbers;
-	int i;
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
-	fibonacci_numbers = fibonacci_sequence(98);
-	if (fibonacci_numbers == NULL)
+	j = 1;
+	k = 2;
+
+	printf("%lu", j);
+
+	for (i = 1; i < 91; i++)
 	{
-		printf("Failed to generate Fibonacci sequence.\n");
-		return (1);
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
 	}
 
-	for (i = 0; i < 98; i++)
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+
+	for (i = 92; i < 99; ++i)
 	{
-		printf("%d", fibonacci_numbers[i]);
-		if (i != 97)
-			printf(", ");
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
 	}
 
 	printf("\n");
-	free(fibonacci_numbers);
 
 	return (0);
 }
