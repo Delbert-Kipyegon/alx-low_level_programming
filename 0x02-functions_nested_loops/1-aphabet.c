f3137057c1becbab3b856f5f9744b98005cb75ad
Delbert-Kipyegon <delbertkip@gmail.com>
2023-06-20 03:32:50 -0700

    alphabet function

---
+++ 0x02-functions_nested_loops/1-aphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints alphabet followed by a new line
+ *
+ * Return: Alway 0 (Sucess)
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a'
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');	
+	
+	return (0);
+}
