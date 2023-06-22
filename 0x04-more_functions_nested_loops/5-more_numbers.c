1c39e216fa140d8127b722502cb5045db725f12f
Delbert-Kipyegon <delbertkip@gmail.com>
2023-06-22 03:55:27 -0700

    0-14 *10

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,19 +1,20 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
-* more_numbers - prints 10 times the numbers, from 0 to 14.
-*/
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * followed by a new line
+ */
 void more_numbers(void)
 {
-	int i;
-	char c;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (c = 0; c <= 14; c++)
+		for (j = 0; j < 15; j++)
 		{
-			if (c > 9)
-			_putchar((c / 10) + '0');
-			_putchar((c % 10) + '0');
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
