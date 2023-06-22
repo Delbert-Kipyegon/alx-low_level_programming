99fc17990075bc3dbd961abf2e1073d9950fbec4
Delbert-Kipyegon <delbertkip@gmail.com>
2023-06-22 03:03:47 -0700

    multiplicator

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers
+ * @a: int a
+ * @b: int b
+ * Return: an integer
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+return (result);
+}
